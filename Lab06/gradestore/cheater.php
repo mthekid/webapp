ca81c00c6bcae144bca2a02dd3cc446306c2be3c
mthekid <gasman87@daum.net>
2019-11-07 21:04:09 +0900

    lab06 success
    finish

--- Lab06/gradestore/cheater.php
+++ Lab06/gradestore/cheater.php
@@ -6,15 +6,38 @@
 	</head>
 
 	<body>
+
+	<p> <?php 
+		// if( preg_match("/^4\d{15}$/" , $_POST["creditCard"]) ) s
+		// if( preg_match("/^\d{16}$/" , $_POST["creditCard"]) ) {
+		// 	print "match credit card";
+		// } else {
+		// 	print "didn't match creditcard number";
+		// } ?>
+<br>
+	<?php
+		// if(preg_match("/^[a-zA-Z]+[\s||\-]?[a-zA-Z]+$/" , $_POST["name"])) {
+		// 	print "match name";
+		// } else {
+		// 	print "didn't match name";
+		// }
+	?> </p>
 		
+		<?php 
+		
+		?>
+
 		<?php
 		# Ex 4 : 
 		# Check the existence of each parameter using the PHP function 'isset'.
 		# Check the blankness of an element in $_POST by comparing it to the empty string.
 		# (can also use the element itself as a Boolean test!)
-		 if (false){
-			// if(empty($_POST["name"]) || empty($_POST["id"]) || !isset($_POST["creditCard"]) 
-			// || !isset($_POST["cc"]) || !isset($_POST["course"]) ) { ?>
+		//  if (false){
+			$flag = true;
+			if(empty($_POST["name"]) || empty($_POST["id"]) || !isset($_POST["creditCard"]) 
+			|| !isset($_POST["cc"]) || !isset($_POST["course"]) ) {
+				$flag = false; 
+				?>
 			<h1>Sorry</h1>
 			<p>You didn't fill out the form completely. <a href="./gradestore.html">Try again?</a></p>
 			<?php 
@@ -33,18 +56,21 @@
 		# test case : 통과 : a martin, a-martin, -martin, mar-tin a, 
 		# 통과X : a  martin, -  martin, 231martin, 231-martin
  		#
-			 } elseif( false ) { 
-				echo $_POST['name']; 
+			 } elseif( !preg_match("/^[a-zA-Z]+[\s||\-]?[a-zA-Z]+$/" , $_POST["name"]))  {
 			 ?>
 				<h1>Sorry</h1>
 				<p>You didn't provide a valid name. <a href="./gradestore.html">Try again?</a></p>
 			 
-			 <?php } elseif( !( preg_match("/d{16}/", $_POST["creditCard"] )) && !isset($_POST["cc"]) ) { 
-				 if( $_POST["cc"] == "visa" && (!preg_match("/^4\d{15}/", $_POST["creditCard"]) ) ) { 
-			echo $_POST["creditCard"];
+			 <?php } elseif( !preg_match("/^\d{16}$/", $_POST["creditCard"] ) ) {
+				 $flag = false; ?> 
+			 <h1>Sorry</h1>
+			 <p>You didn't provide a valid credit card number. <a href="./gradestore.html">Try again?</a></p>
+			<?php	} elseif( $_POST["cc"] == "visa") { 
+				 	if ( !(preg_match("/^4\d{15}$/", $_POST["creditCard"]) ) ) {
+						$flag = false; 
 		 ?>
-		 <h1>Sorry</h1>
-		 <p>You didn't provide a valid Visa credit card number. <a href="./gradestore.html">Try again?</a></p>
+		 	 <h1>Sorry</h1>
+			 <p>You didn't provide a valid visa credit card number. <a href="./gradestore.html">Try again?</a></p>
 		<!-- Ex 5 : 
 			Display the below error message : 
 			<h1>Sorry</h1>
@@ -67,17 +93,14 @@
 
 		<?php
 		# if all the validation and check are passed 
-		 } elseif ( $_POST["cc"] =="master" && (!preg_match("/\[5]{1}\d{15}/", $_POST["crediCard"])) ) { 
-			echo $_POST["creditCard"];
+		 } } elseif ( $_POST["cc"] == "MasterCard") { 
+			 if ( !(preg_match("/^5{1}\d{15}$/", $_POST["creditCard"])) ) {
+				$flag = false; 
 		?>
-		 	<h1>Sorry</h1>
-		 	<p>You didn't provide a valid Master credit card number. <a href="./gradestore.html">Try again?</a></p>
+ 			<h1>Sorry</h1>
+			 <p>You didn't provide a valid master credit card number. <a href="./gradestore.html">Try again?</a></p>
 		 
-		 <?php }  ?>
-		 <h1> Sorry </h1>
-		 <p>You didn't provide a valid credit card number and choosed any credit card. </p>
-		 <?php } else { ?>
-
+			 <?php } } if($flag) { ?>
 		<h1>Thanks, looser!</h1>
 		<p>Your information has been recorded.</p>
 		
@@ -117,7 +140,7 @@
 			<pre><?php echo file_get_contents($filename);?></pre>
 
 		<?php
-		 }
+			 }
 		?>
 		
 		<?php
