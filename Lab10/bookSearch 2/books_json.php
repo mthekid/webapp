1af79f7e49af31daa1577eabb12a808851cc61ed
mthekid <gasman87@daum.net>
2019-12-11 20:13:03 +0900

    success Lab10
    nice!

--- Lab10/bookSearch 2/books_json.php
+++ Lab10/bookSearch 2/books_json.php
@@ -31,32 +31,13 @@ if (!file_exists($BOOKS_FILE)) {
 
 header("Content-type: application/json");
 
-$bookInfo = "";
 print "{\n  \"books\": [\n";
-$lines = file($BOOKS_FILE);
-for ($i=0; $i < count($lines); $i++) { 
-	list($title, $author, $book_category, $year, $price) = explode("|", trim($lines[$i]));
-	// print "{\"category\": \"$book_category\", \"title\": \"$title\", \"author\": \"$author\", \"year\": $year, \"price\": $price }\n";
 
-	if ($book_category == $category) {
-		$bookInfo .= "{\"category\": \"$book_category\", \"title\": \"$title\", \"author\": \"$author\", \"year\": $year, \"price\": $price },\n";
-		// $booksObject->title = $title;
-		// $booksObject->author = $author;
-		// $booksObject->book_category = $book_category;
-		// $booksObject->year = $year;
-		// $booksObject->price = $price;
-
-		// array_push($bookArr, $booksObject);
-		// $myJson = json_encode($bookArr);
-		// echo $myJson;
-	}
-}
-// $bookInfo = substr_replace($bookInfo,'', -2);
-$bookInfo = substr($bookInfo,0, -2);
-print $bookInfo;
 // write a code to : 
 // 1. read the "books.txt"
 // 2. search all the books that matches the given category 
 // 3. generate the result in JSON data format 
+
 print "  ]\n}\n";
+
 ?>
--- Lab10/bookSearch/book_search.js
+++ Lab10/bookSearch/book_search.js
@@ -3,17 +3,20 @@ window.onload = function() {
 	var y = this.getCheckedRadio(x);
 	// this.console.log(x);
 	// this.console.log(y);
+	this.console.log(getCheckedRadio( $$("input") ) );
 
     $("b_xml").onclick=function(){
 			//construct a Prototype Ajax.request object
 			new Ajax.Request("books.php" , { 
 				method: "get",
+				// parameters: { category : getCheckedRadio($$('input[type="radio"]')) },
+				parameters: { category : getCheckedRadio($$("input")) },
 				onSuccess: showBooks_XML,
 				onFailure: ajaxFailed,
 				onException : ajaxFailed
 			});
-
-    }
+	}
+	
     $("b_json").onclick=function(){
 		new Ajax.Request("books_json.php" , {
 			method: "get",
@@ -42,6 +45,7 @@ function showBooks_XML(ajax) {
 	var yearList = response.getElementsByTagName("year");
 	var authorList = response.getElementsByTagName("author");
 	var ul = $("books");
+	console.log(titleList, yearList, authorList);
 	ul.innerHTML ="";
 
 	for (let index = 0; index < titleList.length; index++) {
@@ -50,7 +54,7 @@ function showBooks_XML(ajax) {
 		var author = authorList[index].firstChild.nodeValue;
 
 		var li = document.createElement("li");
-		li.innerHTML = title + author + "(" + year + ")";
+		li.innerHTML = title + ", by" + author + "(" + year + ")";
 		ul.appendChild(li);
 		// console.log(title , year, author);
 	}
@@ -58,8 +62,21 @@ function showBooks_XML(ajax) {
 }
 
 function showBooks_JSON(ajax) {
-	console.warn(ajax.responseText);
-	alert(ajax.responseText);
+	console.log(ajax.responseText);
+	$("books").innerHTML = "";
+	var jsonData = JSON.parse(ajax.responseText);
+	console.log( Object.keys(jsonData.books).length );
+	for (let index = 0; index < jsonData.books.length; index++) {
+		var title = jsonData.books[index].title;
+		var author = jsonData.books[index].author;
+		var year = jsonData.books[index].year;	
+
+		var li = document.createElement("li");
+		li.innerHTML = title + ", by" + author + "(" + year + ")";
+		$("books").appendChild(li);
+	}
+	// var title = jsonData.books[0].title;
+	// console.log(title)
 }
 
 function ajaxFailed(ajax, exception) {
